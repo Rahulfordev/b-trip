741e881ea7c7657ca254cbaf51941ebdc27358bc
rahulfordev <rahulforsms@gmail.com>
2024-05-24 23:14:20 +0600

    navbar design update

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import { FaBars, FaTimes } from "react-icons/fa";
 import { IoIosArrowDown } from "react-icons/io";
 
 import Logo from "../../public/image/logo/logo.png";
+import Link from "next/link";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -21,47 +22,59 @@ const Navbar = () => {
   return (
     <nav className="bg-white text-[#04669F]">
       <div className="max-w-[1292px] mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-16 md:h-24">
+        <div className="flex justify-between items-center h-16 md:h-[68px]">
           <div className="flex-shrink-0 flex items-center">
-            <Image src={Logo} alt="logo" className="w-24 md:w-[141px]" />
+            <Link href={"/"}>
+              <Image
+                src={Logo}
+                alt="logo"
+                className="w-[85px] cursor-pointer"
+              />
+            </Link>
           </div>
           <div className="hidden md:flex md:items-center md:gap-20">
             <div className="hidden md:flex md:space-x-4">
-              <a href="#" className="px-3 py-2 rounded-md text-lg font-medium">
+              <Link
+                href="#"
+                className="px-3 py-2 rounded-md text-sm font-medium"
+              >
                 Home
-              </a>
-              <a href="#" className="px-3 py-2 rounded-md text-lg font-medium">
+              </Link>
+              <Link
+                href="#"
+                className="px-3 py-2 rounded-md text-sm font-medium"
+              >
                 Flight
-              </a>
+              </Link>
               <div className="relative">
                 <button
                   onClick={() => toggleDropdown("tour")}
-                  className="px-3 py-2 rounded-md text-lg font-medium flex items-center gap-2"
+                  className="px-3 py-2 rounded-md text-sm font-medium flex items-center gap-2"
                 >
                   Tour
                   <IoIosArrowDown />
                 </button>
                 {dropdownOpen === "tour" && (
-                  <div className="absolute left-0 mt-7 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                  <div className="absolute left-0 mt-4 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
                     <div className="py-1">
-                      <a
+                      <Link
                         href="#"
                         className="block px-4 py-2 text-sm text-gray-700"
                       >
                         Service 1
-                      </a>
-                      <a
+                      </Link>
+                      <Link
                         href="#"
                         className="block px-4 py-2 text-sm text-gray-700"
                       >
                         Service 2
-                      </a>
-                      <a
+                      </Link>
+                      <Link
                         href="#"
                         className="block px-4 py-2 text-sm text-gray-700"
                       >
                         Service 3
-                      </a>
+                      </Link>
                     </div>
                   </div>
                 )}
@@ -69,32 +82,32 @@ const Navbar = () => {
               <div className="relative">
                 <button
                   onClick={() => toggleDropdown("haj")}
-                  className="px-3 py-2 rounded-md text-lg font-medium flex items-center gap-2"
+                  className="px-3 py-2 rounded-md text-sm font-medium flex items-center gap-2"
                 >
                   Haj
                   <IoIosArrowDown />
                 </button>
                 {dropdownOpen === "haj" && (
-                  <div className="absolute left-0 mt-7 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                  <div className="absolute left-0 mt-4 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
                     <div className="py-1">
-                      <a
+                      <Link
                         href="#"
                         className="block px-4 py-2 text-sm text-gray-700"
                       >
                         Service 1
-                      </a>
-                      <a
+                      </Link>
+                      <Link
                         href="#"
                         className="block px-4 py-2 text-sm text-gray-700"
                       >
                         Service 2
-                      </a>
-                      <a
+                      </Link>
+                      <Link
                         href="#"
                         className="block px-4 py-2 text-sm text-gray-700"
                       >
                         Service 3
-                      </a>
+                      </Link>
                     </div>
                   </div>
                 )}
@@ -102,42 +115,45 @@ const Navbar = () => {
               <div className="relative">
                 <button
                   onClick={() => toggleDropdown("visa")}
-                  className="px-3 py-2 rounded-md text-lg font-medium flex items-center gap-2"
+                  className="px-3 py-2 rounded-md text-sm font-medium flex items-center gap-2"
                 >
                   Visa
                   <IoIosArrowDown />
                 </button>
                 {dropdownOpen === "visa" && (
-                  <div className="absolute left-0 mt-7 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                  <div className="absolute left-0 mt-4 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
                     <div className="py-1">
-                      <a
+                      <Link
                         href="#"
                         className="block px-4 py-2 text-sm text-gray-700"
                       >
                         Service 1
-                      </a>
-                      <a
+                      </Link>
+                      <Link
                         href="#"
                         className="block px-4 py-2 text-sm text-gray-700"
                       >
                         Service 2
-                      </a>
-                      <a
+                      </Link>
+                      <Link
                         href="#"
                         className="block px-4 py-2 text-sm text-gray-700"
                       >
                         Service 3
-                      </a>
+                      </Link>
                     </div>
                   </div>
                 )}
               </div>
-              <a href="#" className="px-3 py-2 rounded-md text-lg font-medium">
+              <Link
+                href="#"
+                className="px-3 py-2 rounded-md text-sm font-medium"
+              >
                 Blog
-              </a>
+              </Link>
             </div>
             <div>
-              <button className="text-xl font-semibold px-7 py-3 rounded-[38px] bg-[#04669F] text-white">
+              <button className="text-sm font-semibold px-7 py-3 rounded-[38px] bg-[#04669F] text-white">
                 Login
               </button>
             </div>
@@ -159,18 +175,18 @@ const Navbar = () => {
       {isOpen && (
         <div className="md:hidden">
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            <a
+            <Link
               href="#"
               className="block px-3 py-2 rounded-md text-base font-medium"
             >
               Home
-            </a>
-            <a
+            </Link>
+            <Link
               href="#"
               className="block px-3 py-2 rounded-md text-base font-medium"
             >
               Flight
-            </a>
+            </Link>
             <div className="relative">
               <button
                 onClick={() => toggleDropdown("tour")}
@@ -182,24 +198,24 @@ const Navbar = () => {
               {dropdownOpen === "tour" && (
                 <div className="mt-2 w-full rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
                   <div className="py-1">
-                    <a
+                    <Link
                       href="#"
                       className="block px-4 py-2 text-sm text-gray-700"
                     >
                       Service 1
-                    </a>
-                    <a
+                    </Link>
+                    <Link
                       href="#"
                       className="block px-4 py-2 text-sm text-gray-700"
                     >
                       Service 2
-                    </a>
-                    <a
+                    </Link>
+                    <Link
                       href="#"
                       className="block px-4 py-2 text-sm text-gray-700"
                     >
                       Service 3
-                    </a>
+                    </Link>
                   </div>
                 </div>
               )}
@@ -215,24 +231,24 @@ const Navbar = () => {
               {dropdownOpen === "haj" && (
                 <div className="mt-2 w-full rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
                   <div className="py-1">
-                    <a
+                    <Link
                       href="#"
                       className="block px-4 py-2 text-sm text-gray-700"
                     >
                       Service 1
-                    </a>
-                    <a
+                    </Link>
+                    <Link
                       href="#"
                       className="block px-4 py-2 text-sm text-gray-700"
                     >
                       Service 2
-                    </a>
-                    <a
+                    </Link>
+                    <Link
                       href="#"
                       className="block px-4 py-2 text-sm text-gray-700"
                     >
                       Service 3
-                    </a>
+                    </Link>
                   </div>
                 </div>
               )}
@@ -248,36 +264,36 @@ const Navbar = () => {
               {dropdownOpen === "visa" && (
                 <div className="mt-2 w-full rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
                   <div className="py-1">
-                    <a
+                    <Link
                       href="#"
                       className="block px-4 py-2 text-sm text-gray-700"
                     >
                       Service 1
-                    </a>
-                    <a
+                    </Link>
+                    <Link
                       href="#"
                       className="block px-4 py-2 text-sm text-gray-700"
                     >
                       Service 2
-                    </a>
-                    <a
+                    </Link>
+                    <Link
                       href="#"
                       className="block px-4 py-2 text-sm text-gray-700"
                     >
                       Service 3
-                    </a>
+                    </Link>
                   </div>
                 </div>
               )}
             </div>
-            <a
+            <Link
               href="#"
               className="block px-3 py-2 rounded-md text-base font-medium"
             >
               Blog
-            </a>
+            </Link>
             <div>
-              <button className="text-lg font-medium px-6 py-2 rounded-[38px] bg-[#04669F] text-white">
+              <button className="text-sm font-medium px-6 py-2 rounded-[38px] bg-[#04669F] text-white">
                 Login
               </button>
             </div>
