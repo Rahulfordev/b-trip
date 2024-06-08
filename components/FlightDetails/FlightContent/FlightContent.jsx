975ca227bf97a611d276bc9b9b53290c8132d593
rahulfordev <rahulforsms@gmail.com>
2024-06-08 06:51:29 +0600

    flight details: flight content responsive complete

--- components/FlightDetails/FlightContent/FlightContent.jsx
+++ components/FlightDetails/FlightContent/FlightContent.jsx
@@ -31,7 +31,7 @@ const FlightContent = () => {
   return (
     <div className="w-full md:w-[1003px] mx-auto">
       <div className="mb-8">
-        <h4 className="text-xl font-semibold text-[#04669F]">
+        <h4 className="text-[15px] md:text-lg lg:text-xl font-semibold text-[#04669F]">
           10 Available Flights
         </h4>
       </div>
@@ -158,19 +158,23 @@ const FlightContent = () => {
               <div className="flex flex-col md:flex-row items-center justify-between">
                 <div className="w-full md:basis-[75%] pb-4 md:pb-0">
                   <div className="flex w-full flex-col md:flex-row items-center justify-between text-[#04669F]">
-                    <div className="flex items-center justify-start w-full pb-3 md:pb-0 md:pl-[8px]">
+                    <div className="flex items-center gap-2 sm:gap-0 justify-start w-full pb-3 md:pb-0 md:pl-[8px]">
                       <div>
                         <Image src={vqf} alt="vqf" />
                       </div>
                       <div>
-                        <h4 className="font-semibold text-xl">NOVOAIR</h4>
+                        <h4 className="font-semibold text-[15px] md:text-lg lg:text-xl">
+                          NOVOAIR
+                        </h4>
                       </div>
                     </div>
                     <div className="md:flex w-full items-center justify-between">
                       <div>
                         <div className="flex w-full items-center gap-8 justify-between">
                           <div>
-                            <h4 className="font-semibold text-xl">10:30</h4>
+                            <h4 className="font-semibold text-[15px] md:text-lg lg:text-xl">
+                              10:30
+                            </h4>
                             <p className="text-sm font-normal">DAC</p>
                           </div>
                           <div className="text-center">
@@ -179,7 +183,9 @@ const FlightContent = () => {
                             <p className="text-sm font-normal">Direct Flight</p>
                           </div>
                           <div>
-                            <h4 className="font-semibold text-xl">11:30</h4>
+                            <h4 className="font-semibold text-[15px] md:text-lg lg:text-xl">
+                              11:30
+                            </h4>
                             <p className="text-sm font-normal">CXB</p>
                           </div>
                         </div>
@@ -196,23 +202,24 @@ const FlightContent = () => {
                     <div>
                       <div className="flex items-center gap-2 justify-start md:justify-end">
                         <Image src={coupon} alt="coupon" />
-                        <p className="text-sm font-medium text-[#04669F]">
+                        <p className="text-[11px] sm:text-sm font-medium text-[#04669F]">
                           AMEX00A5
                         </p>
                       </div>
                       <div className="flex md:block items-center gap-1">
-                        <p className="flex items-center md:justify-end gap-1 text-xl text-[#04669F]">
+                        <p className="flex items-center md:justify-end gap-1 text-[15px] md:text-lg lg:text-xl text-[#04669F]">
                           <span>৳</span>
                           <span className="font-semibold">4,597</span>
                         </p>
-                        <del className="flex md:justify-end text-sm font-normal text-[#04669F]">
+                        <del className="flex md:justify-end text-[11px] sm:text-sm font-normal text-[#04669F]">
                           ৳ 4,999
                         </del>
                       </div>
                     </div>
                     <div>
-                      <button className="flex items-center justify-center md:justify-end gap-1 bg-[#FC9A16] rounded-[10px] text-white text-sm font-normal py-[11px] px-[35px]">
-                        Select <MdKeyboardArrowRight className="text-xl" />
+                      <button className="flex items-center justify-center md:justify-end gap-1 bg-[#FC9A16] rounded-[10px] text-white text-[12px] sm:text-sm font-normal py-[8px] sm:py-[11px] px-[18px] md:px-[26px] lg:px-[35px]">
+                        Select{" "}
+                        <MdKeyboardArrowRight className="text-base sm:text-xl" />
                       </button>
                     </div>
                   </div>
@@ -222,24 +229,24 @@ const FlightContent = () => {
             <div className="w-full">
               <div className="bg-[#F5F5F5] text-[#04669F] text-sm py-3">
                 <div
-                  className="flex items-center justify-between mx-4"
+                  className="flex items-center justify-between mx-1 sm:mx-4"
                   onClick={() => toggleFlightDetails("flight")}
                 >
-                  <div className="flex items-center gap-2">
-                    <div className="flex items-center gap-[6px] bg-[#EDEDED] p-2 rounded-full">
+                  <div className="flex items-center gap-1 sm:gap-2">
+                    <div className="flex items-center gap-[4px] sm:gap-[6px] bg-[#EDEDED] p-1 sm:p-2 rounded-full">
                       <Image src={dimond} alt="dimond" />
-                      <p>Partially Refundable</p>
+                      <p className="text-[10px]">Partially Refundable</p>
                     </div>
-                    <div className="flex items-center gap-[6px] bg-[#EDEDED] p-2 rounded-full">
+                    <div className="flex items-center gap-[4px] sm:gap-[6px] bg-[#EDEDED] p-1 sm:p-2 rounded-full">
                       <Image src={coin} alt="coin" />
-                      <p>100</p>
+                      <p className="text-[10px]">100</p>
                     </div>
                   </div>
                   <div>
-                    <p className="flex items-center gap-1 text-[#FC9A16] text-sm">
+                    <p className="flex items-center sm:gap-1 text-[#FC9A16] text-[11px] sm:text-sm">
                       View Details
                       <MdKeyboardArrowDown
-                        className={`text-lg transition-transform duration-200 ${
+                        className={`text-sm sm:text-lg transition-transform duration-200 ${
                           showFlightDetails.flight ? "rotate-180" : ""
                         }`}
                       />
@@ -248,73 +255,73 @@ const FlightContent = () => {
                 </div>
               </div>
               <div
-                className={`overflow-hidden text-[#04669F] hidden transition-all duration-200 ${
+                className={`overflow-hidden text-[#04669F] transition-all duration-200 ${
                   showFlightDetails.flight
-                    ? "max-h-[480px] md:max-h-96"
+                    ? "max-h-[520px] md:max-h-96"
                     : "max-h-0"
                 }`}
               >
-                <div className="p-3 bg-[#F5F5F5] flex justify-between flex-col md:flex-row">
-                  <div className="bg-white rounded-[10px] p-4 lg:basis-[50%]">
-                    <h4>Flight Details</h4>
+                <div className="p-3 bg-[#F5F5F5] flex flex-col md:flex-row justify-between">
+                  <div className="bg-white rounded-[10px] p-4 w-full md:w-[49%] mb-4 md:mb-0">
+                    <h4 className="text-center text-sm sm:text-base font-semibold mb-1">
+                      Flight Details
+                    </h4>
                     <div>
-                      <div>
-                        <div className="flex justify-center text-center">
-                          <div className="bg-[#FC9A16] relative text-white rounded-[10px] cursor-pointer py-1 px-5 after:absolute after:content-['hello'] after:border-[7px] after:border-transparent after:border-t-[#FC9A16] after:h-0 after:left-[50%] after:top-[100%] after:w-0 after:-translate-x-1/2">
-                            DAC - CXB
-                          </div>
+                      <div className="flex justify-center text-center">
+                        <div className="bg-[#FC9A16] text-sm relative text-white rounded-[10px] cursor-pointer py-1 px-5 after:absolute after:content-[''] after:border-[7px] after:border-transparent after:border-t-[#FC9A16] after:h-0 after:left-[50%] after:top-[100%] after:w-0 after:-translate-x-1/2">
+                          DAC - CXB
                         </div>
                       </div>
-                      <div>
+                      <div className="mt-4 flex items-center gap-1 justify-between">
+                        <div className="flex flex-col sm:flex-row sm:items-center gap-1">
+                          <Image src={novoair} alt="novoair logo" />
+                          <div className="text-sm">
+                            <span className="block">NOVOAIR</span>
+                            <span className="block">VQ 923 | ATR725</span>
+                          </div>
+                        </div>
                         <div>
-                          <div className="mt-4 flex items-center justify-between">
-                            <div>
-                              <div>
-                                <Image src={novoair} alt="novoair logo" />
-                              </div>
-                              <div>
-                                <span>NOVOAIR</span>
-                                <span>VQ 923 | ATR725</span>
-                              </div>
-                            </div>
-                            <div>
-                              <p>Class: Economy</p>
-                            </div>
-                          </div>
-                          <div className="mt-3 border-t pt-3 flex items-center justify-between">
-                            <div>
-                              <h4 className="font-semibold text-xl">10:30</h4>
-                              <span className="text-sm font-normal">
-                                Sat, 04 Apr, 2024
-                              </span>
-                              <span className="text-sm font-normal">DAC</span>
-                            </div>
-                            <div>
-                              <h4 className="font-semibold text-xl">11:30</h4>
-                              <span className="text-sm font-normal">
-                                Sat, 04 Apr, 2024
-                              </span>
-                              <span className="text-sm font-normal">CXB</span>
-                            </div>
-                          </div>
+                          <p className="text-sm flex flex-wrap gap-1">
+                            Class: <span>Economy</span>
+                          </p>
+                        </div>
+                      </div>
+                      <div className="mt-3 border-t pt-3 flex items-center justify-between">
+                        <div>
+                          <h4 className="font-semibold text-[15px] md:text-lg lg:text-xl">
+                            10:30
+                          </h4>
+                          <span className="text-sm font-normal block">
+                            Sat, 04 Apr, 2024
+                          </span>
+                          <span className="text-sm font-normal">DAC</span>
+                        </div>
+                        <div>
+                          <h4 className="font-semibold text-[15px] md:text-lg lg:text-xl">
+                            11:30
+                          </h4>
+                          <span className="text-sm font-normal block">
+                            Sat, 04 Apr, 2024
+                          </span>
+                          <span className="text-sm font-normal">CXB</span>
                         </div>
                       </div>
                     </div>
                   </div>
-                  <div className="bg-[#EDEDED] rounded-[10px] lg:basis-[50%]">
+                  <div className="bg-[#EDEDED] rounded-[10px] w-full md:w-[49%]">
                     <div className="p-4">
-                      <div className="flex items-center justify-between">
-                        <button className="text-sm font-medium text-white bg-[#FC9A16] py-2 px-[47px] rounded-tl-[10px] rounded-bl-[10px]">
+                      <div className="flex items-center gap-[2px] justify-between">
+                        <button className="text-sm font-medium text-white bg-[#FC9A16] py-2 w-full rounded-tl-[10px] rounded-bl-[10px]">
                           Baggage
                         </button>
-                        <button className="text-sm font-medium bg-[#D9D9D9] py-2 px-[47px]">
+                        <button className="text-sm font-medium bg-[#D9D9D9] py-2 w-full">
                           Fare
                         </button>
-                        <button className="text-sm font-medium bg-[#D9D9D9] py-2 px-[47px] rounded-tr-[10px] rounded-br-[10px]">
+                        <button className="text-sm font-medium bg-[#D9D9D9] py-2 w-full rounded-tr-[10px] rounded-br-[10px]">
                           Policy
                         </button>
                       </div>
-                      <div className="mt-3 mb-[58px]">
+                      <div className="mt-3 mb-4 md:mb-[58px]">
                         <div className="flex items-center justify-between text-sm">
                           <span>Flight</span>
                           <span>Cabin</span>
